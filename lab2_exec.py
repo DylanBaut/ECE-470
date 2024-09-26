6614ca3504102ba44b495cae0a2ada74fd3383b4
Dylan Bautista <70183425+DylanBaut@users.noreply.github.com>
2024-09-26 16:54:49 -0500

    Update lab2_exec.py
    Need to do gripper

--- lab2_exec.py
+++ lab2_exec.py
@@ -25,9 +25,27 @@ SPIN_RATE = 20
 home = np.radians([120, -90, 90, -90, -90, 0])
 
 # Hanoi tower location 1
-Q11 = [120*pi/180.0, -56*pi/180.0, 124*pi/180.0, -158*pi/180.0, -90*pi/180.0, 0*pi/180.0]
-Q12 = [120*pi/180.0, -64*pi/180.0, 123*pi/180.0, -148*pi/180.0, -90*pi/180.0, 0*pi/180.0]
-Q13 = [120*pi/180.0, -72*pi/180.0, 120*pi/180.0, -137*pi/180.0, -90*pi/180.0, 0*pi/180.0]
+# Q11 = [120*pi/180.0, -56*pi/180.0, 124*pi/180.0, -158*pi/180.0, -90*pi/180.0, 0*pi/180.0]
+# Q12 = [120*pi/180.0, -64*pi/180.0, 123*pi/180.0, -148*pi/180.0, -90*pi/180.0, 0*pi/180.0]
+# Q13 = [120*pi/180.0, -72*pi/180.0, 120*pi/180.0, -137*pi/180.0, -90*pi/180.0, 0*pi/180.0]
+#tower 1
+Q10 = [174.65*pi/180.0, -102.68*pi/180.0, 124.10*pi/180.0, -111.25*pi/180.0, -89.82*pi/180.0, 54.40*pi/180.0]
+Q11 = [174.50*pi/180.0, -87.18*pi/180.0, 139.18*pi/180.0, -141.82*pi/180.0, -89.91*pi/180.0, 54.42*pi/180.0]
+Q12 = [174.47*pi/180.0, -76.46*pi/180.0, 142.56*pi/180.0, -155.93*pi/180.0, -89.97*pi/180.0, 54.45*pi/180.0]
+Q13 = [174.69*pi/180.0, -63.74*pi/180.0, 143.86*pi/180.0, -169.94*pi/180.0, -90.03*pi/180.0, 54.68*pi/180.0]
+
+#tower 2
+Q20 = [194.04*pi/180.0, -88.80*pi/180.0, 119.68*pi/180.0, -120.67*pi/180.0, -89.74*pi/180.0, 73.85*pi/180.0]
+Q21 = [193.90*pi/180.0, -78.88*pi/180.0, 128.63*pi/180.0, -139.55*pi/180.0, -89.81*pi/180.0, 73.81*pi/180.0]
+Q22 = [193.40*pi/180.0, -69.38*pi/180.0, 131.02*pi/180.0, -151.42*pi/180.0, -89.86*pi/180.0, 73.36*pi/180.0]
+Q23 = [193.86*pi/180.0, -61.11*pi/180.0, 132.76*pi/180.0, -161.44*pi/180.0, -89.90*pi/180.0, 73.87*pi/180.0]
+
+Q30 = [207.69*pi/180.0, -78.95*pi/180.0, 104.77*pi/180.0, -115.59*pi/180.0, -89.65*pi/180.0, 87.46*pi/180.0]
+Q31 = [207.19*pi/180.0, -68.84*pi/180.0, 114.64*pi/180.0, -135.57*pi/180.0, -89.71*pi/180.0, 87.10*pi/180.0]
+Q32 = [207.17*pi/180.0, -62.69*pi/180.0, 116.85*pi/180.0, -143.93*pi/180.0, -89.74*pi/180.0, 87.13*pi/180.0]
+Q33 = [207.16*pi/180.0, -54.94*pi/180.0, 117.96*pi/180.0, -152.78*pi/180.0, -89.78*pi/180.0, 87.15*pi/180.0]
+
+
 
 thetas = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
 
@@ -47,9 +65,9 @@ current_position = copy.deepcopy(home)
 TODO: Initialize Q matrix
 """
 
-Q = [ [Q11, Q12, Q13], \
-      [Q11, Q12, Q13], \
-      [Q11, Q12, Q13] ]
+Q = [ [Q10, Q11, Q12, Q13], \
+      [Q20, Q21, Q22, Q23], \
+      [Q30, Q31, Q32, Q33] ]
 ############### Your Code End Here ###############
 
 ############## Your Code Start Here ##############
@@ -58,7 +76,8 @@ Q = [ [Q11, Q12, Q13], \
 TODO: define a ROS topic callback funtion for getting the state of suction cup
 Whenever ur3/gripper_input publishes info this callback function is called.
 """
-
+def gripper_callback(msg):
+    pass
 
 
 
@@ -179,16 +198,34 @@ def move_arm(pub_cmd, loop_rate, dest, vel, accel):
 
 
 ############## Your Code Start Here ##############
-
+#loc is tower: 0,1,2
+# height is block (highest) 0(above tower), 1,2,3 (lowest)
 def move_block(pub_cmd, loop_rate, start_loc, start_height, \
                end_loc, end_height):
     global Q
+    start_dest = Q[start_loc][start_height]
+    end_dest=  Q[end_loc][end_height]
 
-    ### Hint: Use the Q array to map out your towers by location and "height".
+    #move above start tower
+    move_arm(pub_cmd, loop_rate, Q[start_loc][0], 4.0, 4.0)
+    move_arm(pub_cmd, loop_rate, start_dest, 4.0, 4.0)
+    #pick up
+    gripper(pub_cmd, loop_rate, suction_on)
+    # Delay to make sure suction cup has grasped the block
+    time.sleep(1.0)
+    move_arm(pub_cmd, loop_rate, Q[start_loc][0], 4.0, 4.0)
+
+
+    #move above end tower
+    move_arm(pub_cmd, loop_rate, Q[end_loc][0], 4.0, 4.0)
+    move_arm(pub_cmd, loop_rate, end_dest, 4.0, 4.0)
+    #drop off
+    gripper(pub_cmd, loop_rate, suction_off)
+    # Delay to make sure suction cup has grasped the block
+    time.sleep(1.0)
+    move_arm(pub_cmd, loop_rate, Q[end_loc][0], 4.0, 4.0)
 
     error = 0
-
-
 
     return error
 
@@ -225,27 +262,34 @@ def main():
     input_done = 0
     loop_count = 0
     ABtower = 0
-    while(not input_done):
-        input_string = input("Enter number of loops <Either 1 2 3 or 0 to quit> ")
-        print("You entered " + input_string + "\n")
+    start_tower = 0
+    mid_tower = 0
+    end_tower = 0
+    # while(not input_done):
+    #     input_string = input("Enter number of loops <Either 1 2 3 or 0 to quit> ")
+    #     print("You entered " + input_string + "\n")
 
-        if(int(input_string) == 1):
-            input_done = 1
-            loop_count = 1
-        elif (int(input_string) == 2):
-            input_done = 1
-            loop_count = 2
-        elif (int(input_string) == 3):
-            input_done = 1
-            loop_count = 3
-        elif (int(input_string) == 0):
-            print("Quitting... ")
-            sys.exit()
-        else:
-            print("Please just enter the character 1 2 3 or 0 to quit \n\n")
+
+    #     if(int(input_string) == 1):
+    #         input_done = 1
+    #         loop_count = 1
+    #     elif (int(input_string) == 2):
+    #         input_done = 1
+    #         loop_count = 2
+    #     elif (int(input_string) == 3):
+    #         input_done = 1
+    #         loop_count = 3
+    #     elif (int(input_string) == 0):
+    #         print("Quitting... ")
+    #         sys.exit()
+    #     else:
+    #         print("Please just enter the character 1 2 3 or 0 to quit \n\n")
     while(not ABtower):
-        ABtower = input("Enter Starting and End Tower")
+        ABtower = input("Enter Starting and End Tower (1,2,3)")
         ABtower = ABtower.strip()
+        start_tower = int(ABtower[0])-1
+        end_tower = int(ABtower[-1])-1
+        mid_tower = 3 -start_tower - end_tower
 
 
 
@@ -263,26 +307,36 @@ def main():
     ############## Your Code Start Here ##############
     # TODO: modify the code so that UR3 can move tower accordingly from user input
 
-    while(loop_count > 0):
+        
+    move_block(pub_command, loop_rate, start_tower, 1, end_tower, 3)
+    move_block(pub_command, loop_rate, start_tower, 2, mid_tower, 3)
+    move_block(pub_command, loop_rate, end_tower, 3, mid_tower, 2)
+    move_block(pub_command, loop_rate, start_tower, 3, end_tower, 3)
+    move_block(pub_command, loop_rate, mid_tower, 2, start_tower, 3)
+    move_block(pub_command, loop_rate, mid_tower, 3, end_tower, 2)
+    move_block(pub_command, loop_rate, start_tower, 3, end_tower, 1)
 
-        move_arm(pub_command, loop_rate, home, 4.0, 4.0)
+    
+    # while(loop_count > 0):
 
-        rospy.loginfo("Sending goal 1 ...")
-        move_arm(pub_command, loop_rate, Q[0][0], 4.0, 4.0)
+    #     move_arm(pub_command, loop_rate, home, 4.0, 4.0)
 
-        gripper(pub_command, loop_rate, suction_on)
-        # Delay to make sure suction cup has grasped the block
-        time.sleep(1.0)
+    #     rospy.loginfo("Sending goal 1 ...")
+    #     move_arm(pub_command, loop_rate, Q[0][0], 4.0, 4.0)
 
-        rospy.loginfo("Sending goal 2 ...")
-        move_arm(pub_command, loop_rate, Q[1][1], 4.0, 4.0)
+    #     gripper(pub_command, loop_rate, suction_on)
+    #     # Delay to make sure suction cup has grasped the block
+    #     time.sleep(1.0)
 
-        rospy.loginfo("Sending goal 3 ...")
-        move_arm(pub_command, loop_rate, Q[2][0], 4.0, 4.0)
+    #     rospy.loginfo("Sending goal 2 ...")
+    #     move_arm(pub_command, loop_rate, Q[1][1], 4.0, 4.0)
 
-        loop_count = loop_count - 1
+    #     rospy.loginfo("Sending goal 3 ...")
+    #     move_arm(pub_command, loop_rate, Q[2][0], 4.0, 4.0)
 
-    gripper(pub_command, loop_rate, suction_off)
+    #     loop_count = loop_count - 1
+
+    # gripper(pub_command, loop_rate, suction_off)
 
 
 
