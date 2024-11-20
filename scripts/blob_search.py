c3e482cb6f929b57062780cbbee822d1a4cc718f
charlsgee <39540857+charlsgee@users.noreply.github.com>
2024-11-19 19:40:01 -0600

    Add files via upload

---
+++ scripts/blob_search.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python
+
+import cv2
+import numpy as np
+
+# ========================= Student's code starts here =========================
+
+# Params for camera calibration
+theta = 0.0
+beta = 0.0
+tx = 0.0
+ty = 0.0
+
+# Function that converts image coord to world coord
+def IMG2W(col, row):
+    Beta = 760
+    theta = 1.508*np.pi/180
+    Ty =.09
+    Tx =.300
+
+    Or= 239
+    Oc = 319
+    xc = (row-Or)/Beta 
+    yc=  (col-Oc)/Beta
+    # print("XC,YC: ",xc,yc, "\n \n \n")
+    xw = (xc+Tx)*np.cos(theta)-(yc+Ty)*np.sin(theta)
+    yw = (xc+Tx)*np.sin(theta)+(yc+Ty)*np.cos(theta)
+    # print("World Coordinates:",xw,",",yw)
+    return(xw,yw)
+
+
+
+
+# ========================= Student's code ends here ===========================
+
+def blob_search(image_raw, color):
+
+    # Setup SimpleBlobDetector parameters.
+    params = cv2.SimpleBlobDetector_Params()
+
+    # ========================= Student's code starts here =========================
+
+    # Filter by Color
+    params.filterByColor = False
+
+    # Filter by Area.
+    params.filterByArea = True
+
+    # Filter by Circularity
+    params.filterByCircularity = False
+
+    # Filter by Inerita
+    params.filterByInertia = False
+
+    # Filter by Convexity
+    params.filterByConvexity = False
+
+    # ========================= Student's code ends here ===========================
+
+    # Create a detector with the parameters
+    detector = cv2.SimpleBlobDetector_create(params)
+
+    # Convert the image into the HSV color space
+    hsv_image = cv2.cvtColor(image_raw, cv2.COLOR_BGR2HSV)
+    # ========================= Student's code starts here =========================
+    
+
+    # print(H_l,S_l,V_l)
+    # print(hsv_upper,"\n",hsv_lower)
+    # lowerhsl =(50,100,75.75)#(H_l,S_l,V_l) #cv2.cvtColor(lower,cv2.COLOR_BGR2HSV)
+    # upperhsl =(64,196,130)#cv2.cvtColor(upper,cv2.COLOR_BGR2HSV)
+
+
+    if(color == 'green'):
+        lowerhsl =(40,70,70)
+        upperhsl =(60,255,255)
+    elif(color == 'yellow'):
+        lowerhsl =(20,102,102)
+        upperhsl =(30,255,255)
+    
+
+    # Define a mask using the lower and upper bounds of the target color
+    mask_image = cv2.inRange(hsv_image, lowerhsl,upperhsl)
+
+    # ========================= Student's code ends here ===========================
+
+    keypoints = detector.detect(mask_image)
+
+    # Find blob centers in the image coordinates
+    blob_image_center = []
+    num_blobs = len(keypoints)
+    for i in range(num_blobs):
+        blob_image_center.append((keypoints[i].pt[0],keypoints[i].pt[1]))
+
+    # ========================= Student's code starts here =========================
+
+    # Draw the keypoints on the detected block
+    # print("KEYPOINTS", keypoints)
+    im_with_keypoints = cv2.drawKeypoints(image_raw, keypoints ,np.array([]),cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)# (0, 255, 0), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
+    # ========================= Student's code ends here ===========================
+
+    xw_yw = []
+
+    if(num_blobs == 0):
+        # print("No block found!")
+        pass
+    else:
+        # Convert image coordinates to global world coordinate using IM2W() function
+        for i in range(num_blobs):
+            xw_yw.append(IMG2W(blob_image_center[i][0], blob_image_center[i][1]))
+
+
+    cv2.namedWindow("Camera View")
+    cv2.imshow("Camera View", image_raw)
+    cv2.namedWindow("Mask View")
+    cv2.imshow("Mask View", mask_image)
+    cv2.namedWindow("Keypoint View")
+    cv2.imshow("Keypoint View", im_with_keypoints)
+
+    cv2.waitKey(2)  
+
+    return xw_yw
---
+++ scripts/lab5_func.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+import numpy as np
+from scipy.linalg import expm, logm
+from lab5_header import*
+
+"""
+Use 'expm' for matrix exponential.
+Angles are in radian, distance are in meters.
+"""
+def Get_MS():
+	# =================== Your code starts here ====================#
+	# Fill in the correct values for w1~6 and v1~6, as well as the M matrix
+	M = np.eye(4)
+	S = np.zeros((6,6))
+
+
+
+
+	# ==============================================================#
+	return M, S
+
+
+"""
+Function that calculates encoder numbers for each motor
+"""
+def lab_fk(theta1, theta2, theta3, theta4, theta5, theta6):
+
+	# Initialize the return_value
+	return_value = [None, None, None, None, None, None]
+
+	print("Foward kinematics calculated:\n")
+
+	# =================== Your code starts here ====================#
+	theta = np.array([theta1,theta2,theta3,theta4,theta5,theta6])
+	T = np.eye(4)
+
+	M, S = Get_MS()
+
+
+
+
+	# ==============================================================#
+
+	return_value[0] = theta1+PI
+	return_value[1] = theta2
+	return_value[2] = theta3
+	return_value[3] = theta4-(0.5*PI)
+	return_value[4] = theta5
+	return_value[5] = theta6
+
+	return return_value
+
+
+"""
+Function that calculates an elbow up Inverse Kinematic solution for the UR3
+"""
+def lab_invk(xWgrip, yWgrip, zWgrip, yaw_WgripDegree):
+	# =================== Your code starts here ====================#
+	l1 = 0.152
+	l2 = 0.12
+	l3 = 0.244
+	l4 = 0.093
+	l5 = 0.213
+	l6 = 0.083
+	l7 = 0.083
+	l8 = 0.082
+	l9 = 0.053
+	l10=  0.05
+	xgoal = xWgrip+.150 
+	ygoal = yWgrip-.150
+	zgoal = zWgrip-.010
+	yaw_W_Rads = np.deg2rad(yaw_WgripDegree)
+
+	xcen = xgoal-l9*np.cos(yaw_W_Rads)
+	ycen = ygoal-l9*np.sin(yaw_W_Rads)
+	zcen = zgoal
+	# atheta = np.arccos((l2**2-(l3+l5+l7)**2-(xcen**2+ycen**2))/(-2*(l3+l5+l7)*(np.sqrt(xcen**2+ycen**2)))) 
+	# c1 = np.sqrt((l7**2)+(.027+l6)**2)
+	offset = (l2-l4+l6)
+	c_theta1 = np.sqrt(xcen**2+ycen**2)
+	atheta = np.arcsin(offset/c_theta1)
+	theta1 = np.arctan2(ycen, xcen)-atheta
+	# x3 = .137*np.sin(.6405-theta1)-xcen
+	to_virtual = np.sqrt(xcen**2+ycen**2-offset**2)-l7
+	x3 = np.cos(theta1)*to_virtual
+	y3 = np.sin(theta1)*to_virtual
+	z3 = zcen+l8+l10
+	# print(x3,y3,z3)
+	th2C = np.arctan2(z3-l1,(x3**2+y3**2))
+	dist_to_eff = np.sqrt(x3**2+y3**2+(z3-l1)**2)
+	smalltheta2 = np.arccos((l3**2+dist_to_eff**2-l5**2)/(2*l3*dist_to_eff))
+
+	theta2 =-(th2C+smalltheta2)
+	theta3 = np.pi-np.arccos((dist_to_eff**2-l3**2-l5**2)/(-2*l3*l5))
+	theta4 =-(theta2+theta3)
+	theta5 =-np.pi/2  
+	theta6 = np.pi/2+theta1-yaw_W_Rads
+
+	print(theta1,"\n",theta2,"\n",theta3,"\n",theta4,"\n",theta5,"\n",theta6)
+
+	# ==============================================================#
+	return lab_fk(theta1, theta2, theta3, theta4, theta5, theta6)
