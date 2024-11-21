ce41884008b790a42dc02af563831abc50f9c670
charlsgee <39540857+charlsgee@users.noreply.github.com>
2024-11-21 15:05:47 -0600

    Update lab2_exec.py

--- lab2_exec.py
+++ lab2_exec.py
@@ -1,90 +1,66 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
-'''
-We get inspirations of Tower of Hanoi algorithm from the website below.
-This is also on the lab manual.
-Source: https://www.cut-the-knot.org/recurrence/hanoi.shtml
-'''
-
-import os
-import argparse
+import sys
 import copy
 import time
 import rospy
-import rospkg
+
 import numpy as np
-import yaml
-import sys
-from math import pi
-from lab2_header import *
+from lab5_header import *
+from lab5_func import *
+from blob_search import *
+
+
+# ========================= Student's code starts here =========================
+
+# Position for UR3 not blocking the camera
+go_away = [270*PI/180.0, -90*PI/180.0, 90*PI/180.0, -90*PI/180.0, -90*PI/180.0, 135*PI/180.0]
+
+# Store world coordinates of green and yellow blocks
+xw_yw_G = []
+xw_yw_Y = []
+
+# Any other global variable you want to define
+# Hints: where to put the blocks?
+
+
+# ========================= Student's code ends here ===========================
+
+################ Pre-defined parameters and functions no need to change below ################
 
 # 20Hz
 SPIN_RATE = 20
 
 # UR3 home location
-home = np.radians([120, -90, 90, -90, -90, 0])
-
-# Hanoi tower location 1
-# Q11 = [120*pi/180.0, -56*pi/180.0, 124*pi/180.0, -158*pi/180.0, -90*pi/180.0, 0*pi/180.0]
-# Q12 = [120*pi/180.0, -64*pi/180.0, 123*pi/180.0, -148*pi/180.0, -90*pi/180.0, 0*pi/180.0]
-# Q13 = [120*pi/180.0, -72*pi/180.0, 120*pi/180.0, -137*pi/180.0, -90*pi/180.0, 0*pi/180.0]
-#tower 1
-Q10 = [174.65*pi/180.0, -102.68*pi/180.0, 124.10*pi/180.0, -111.25*pi/180.0, -89.82*pi/180.0, 54.40*pi/180.0]
-Q11 = [174.50*pi/180.0, -87.18*pi/180.0, 139.18*pi/180.0, -141.82*pi/180.0, -89.91*pi/180.0, 54.42*pi/180.0]
-Q12 = [174.47*pi/180.0, -76.46*pi/180.0, 142.56*pi/180.0, -155.93*pi/180.0, -89.97*pi/180.0, 54.45*pi/180.0]
-Q13 = [174.69*pi/180.0, -63.74*pi/180.0, 143.86*pi/180.0, -169.94*pi/180.0, -90.03*pi/180.0, 54.68*pi/180.0]
-
-#tower 2
-Q20 = [194.04*pi/180.0, -88.80*pi/180.0, 119.68*pi/180.0, -120.67*pi/180.0, -89.74*pi/180.0, 73.85*pi/180.0]
-Q21 = [193.90*pi/180.0, -78.88*pi/180.0, 128.63*pi/180.0, -139.55*pi/180.0, -89.81*pi/180.0, 73.81*pi/180.0]
-Q22 = [193.40*pi/180.0, -69.38*pi/180.0, 131.02*pi/180.0, -151.42*pi/180.0, -89.86*pi/180.0, 73.36*pi/180.0]
-Q23 = [193.86*pi/180.0, -61.11*pi/180.0, 132.76*pi/180.0, -161.44*pi/180.0, -89.90*pi/180.0, 73.87*pi/180.0]
-
-Q30 = [207.69*pi/180.0, -78.95*pi/180.0, 104.77*pi/180.0, -115.59*pi/180.0, -89.65*pi/180.0, 87.46*pi/180.0]
-Q31 = [207.19*pi/180.0, -68.84*pi/180.0, 114.64*pi/180.0, -135.57*pi/180.0, -89.71*pi/180.0, 87.10*pi/180.0]
-Q32 = [207.17*pi/180.0, -62.69*pi/180.0, 116.85*pi/180.0, -143.93*pi/180.0, -89.74*pi/180.0, 87.13*pi/180.0]
-Q33 = [207.16*pi/180.0, -54.94*pi/180.0, 117.96*pi/180.0, -152.78*pi/180.0, -89.78*pi/180.0, 87.15*pi/180.0]
-
-
-
-thetas = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
-gripper_in = 1
-
-digital_in_0 = 0
-analog_in_0 = 0
-
-suction_on = True
-suction_off = False
-current_io_0 = False
-current_position_set = False
+home = [0*PI/180.0, 0*PI/180.0, 0*PI/180.0, 0*PI/180.0, 0*PI/180.0, 0*PI/180.0]
 
 # UR3 current position, using home position for initialization
 current_position = copy.deepcopy(home)
 
-############## Your Code Start Here ##############
-"""
-TODO: Initialize Q matrix
-"""
+thetas = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
 
-Q = [ [Q10, Q11, Q12, Q13], \
-      [Q20, Q21, Q22, Q23], \
-      [Q30, Q31, Q32, Q33] ]
-############### Your Code End Here ###############
+digital_in_0 = 0
+analog_in_0 = 0.0
 
-############## Your Code Start Here ##############
+suction_on = True
+suction_off = False
+
+current_io_0 = False
+current_position_set = False
+
+image_shape_define = False
+
 
 """
-TODO: define a ROS topic callback funtion for getting the state of suction cup
 Whenever ur3/gripper_input publishes info this callback function is called.
 """
-def gripper_callback(msg):
+def input_callback(msg):
+
+    global digital_in_0
     global gripper_in
-    gripper_in =msg.AIN0
-    pass
-
-
-
-############### Your Code End Here ###############
+    gripper_in = msg.AIN0
+    digital_in_0 = msg.DIGIN
+    digital_in_0 = digital_in_0 & 1 # Only look at least significant bit, meaning index 0
 
 
 """
@@ -113,6 +89,9 @@ def position_callback(msg):
     current_position_set = True
 
 
+"""
+Function to control the suction cup on/off
+"""
 def gripper(pub_cmd, loop_rate, io_0):
 
     global SPIN_RATE
@@ -142,6 +121,7 @@ def gripper(pub_cmd, loop_rate, io_0):
             abs(thetas[4]-driver_msg.destination[4]) < 0.0005 and \
             abs(thetas[5]-driver_msg.destination[5]) < 0.0005 ):
 
+            #rospy.loginfo("Goal is reached!")
             at_goal = 1
 
         loop_rate.sleep()
@@ -157,6 +137,9 @@ def gripper(pub_cmd, loop_rate, io_0):
     return error
 
 
+"""
+Move robot arm from one position to another
+"""
 def move_arm(pub_cmd, loop_rate, dest, vel, accel):
 
     global thetas
@@ -185,7 +168,7 @@ def move_arm(pub_cmd, loop_rate, dest, vel, accel):
             abs(thetas[5]-driver_msg.destination[5]) < 0.0005 ):
 
             at_goal = 1
-            rospy.loginfo("Goal is reached!")
+            #rospy.loginfo("Goal is reached!")
 
         loop_rate.sleep()
 
@@ -199,171 +182,173 @@ def move_arm(pub_cmd, loop_rate, dest, vel, accel):
 
     return error
 
+################ Pre-defined parameters and functions no need to change above ################
 
-############## Your Code Start Here ##############
-#loc is tower: 0,1,2
-# height is block (highest) 0(above tower), 1,2,3 (lowest)
-def move_block(pub_cmd, loop_rate, start_loc, start_height, \
-               end_loc, end_height):
-    global Q
+
+def move_block(pub_cmd, loop_rate, start_xw_yw_zw, target_xw_yw_zw, vel, accel):
+
+    """
+    start_xw_yw_zw: where to pick up a block in global coordinates
+    target_xw_yw_zw: where to place the block in global coordinates
+
+    hint: you will use lab_invk(), gripper(), move_arm() functions to
+    pick and place a block
+
+    """
+    # ========================= Student's code starts here =========================
+
+    # global variable1
+    # global variable2
+    zval = .03
+    # print("start loop")
+    start_theta = lab_invk(start_xw_yw_zw[0],start_xw_yw_zw[1],zval+.05,0)
+    move_arm(pub_cmd, loop_rate, start_theta, vel, accel) #move above block
+    start_theta2 = lab_invk(start_xw_yw_zw[0],start_xw_yw_zw[1],zval,0)
+    time.sleep(.1)
+    move_arm(pub_cmd, loop_rate, start_theta2, vel, accel) #move above block
+    gripper(pub_cmd, loop_rate, suction_on)#suction on
+
+    nextset = lab_invk(start_xw_yw_zw[0],start_xw_yw_zw[1],zval+.05,0)
+    time.sleep(.2)
+    
+    move_arm(pub_cmd, loop_rate, nextset, vel, accel) #move above block
+    time.sleep(.1)
+    target_theta = lab_invk(target_xw_yw_zw[0],target_xw_yw_zw[1],zval,0)
+    move_arm(pub_cmd, loop_rate, target_theta, vel, accel) #move above destination
+    # move_arm(pub_cmd, loop_rate, , vel, accel) #move to destination
+    time.sleep(.2)
+    gripper(pub_cmd, loop_rate, suction_off)               #suction off
+    nextset = lab_invk(start_xw_yw_zw[0],start_xw_yw_zw[1],zval+.05,0)
+    move_arm(pub_cmd, loop_rate, nextset, vel, accel) #move above block
     error = 0
-    success = 1
-    start_dest = Q[start_loc][start_height]
-    end_dest=  Q[end_loc][end_height]
 
-    #move above start tower
-    move_arm(pub_cmd, loop_rate, Q[start_loc][0], 4.0, 4.0)
-    move_arm(pub_cmd, loop_rate, start_dest, 4.0, 4.0)
-    #pick up
-    gripper(pub_cmd, loop_rate, suction_on)
-    # Delay to make sure suction cup has grasped the block
-    time.sleep(1.0)
-    move_arm(pub_cmd, loop_rate, Q[start_loc][0], 4.0, 4.0)
-    if gripper_in < 1.95:
-        gripper(pub_cmd, loop_rate, suction_off)
-        return error
+    # ========================= Student's code ends here ===========================
 
-    #move above end tower
-    move_arm(pub_cmd, loop_rate, Q[end_loc][0], 4.0, 4.0)
-    move_arm(pub_cmd, loop_rate, end_dest, 4.0, 4.0)
-    #drop off
-    gripper(pub_cmd, loop_rate, suction_off)
-    # Delay to make sure suction cup has grasped the block
-    time.sleep(1.0)
-    move_arm(pub_cmd, loop_rate, Q[end_loc][0], 4.0, 4.0)
+    return error
 
 
-    return success
+class ImageConverter:
+
+    def __init__(self, SPIN_RATE):
+
+        self.bridge = CvBridge()
+        self.image_pub = rospy.Publisher("/image_converter/output_video", Image, queue_size=10)
+        self.image_sub = rospy.Subscriber("/cv_camera_node/image_raw", Image, self.image_callback)
+        self.loop_rate = rospy.Rate(SPIN_RATE)
+
+        # Check if ROS is ready for operation
+        while(rospy.is_shutdown()):
+            print("ROS is shutdown!")
 
 
-############### Your Code End Here ###############
+    def image_callback(self, data):
+
+        global xw_yw_G # store found green blocks in this list
+        global xw_yw_Y # store found yellow blocks in this list
+
+        try:
+          # Convert ROS image to OpenCV image
+            raw_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+        except CvBridgeError as e:
+            print(e)
+
+        cv_image = cv2.flip(raw_image, -1)
+        cv2.line(cv_image, (0,50), (640,50), (0,0,0), 5)
+
+        # You will need to call blob_search() function to find centers of green blocks
+        # and yellow blocks, and store the centers in xw_yw_G & xw_yw_Y respectively.
+
+        # If no blocks are found for a particular color, you can return an empty list,
+        # to xw_yw_G or xw_yw_Y.
+
+        # Remember, xw_yw_G & xw_yw_Y are in global coordinates, which means you will
+        # do coordinate transformation in the blob_search() function, namely, from
+        # the image frame to the global world frame.
+
+        xw_yw_G = blob_search(cv_image, "green")
+        xw_yw_Y = blob_search(cv_image, "yellow")
 
 
+"""
+Program run from here
+"""
 def main():
 
-    global home
-    global Q
-    global SPIN_RATE
+    global go_away
+    global xw_yw_Y
+    global xw_yw_G
+    global xw_yw_R
+
+    # global variable1
+    # global variable2
 
     # Initialize ROS node
-    rospy.init_node('lab2node')
+    rospy.init_node('lab5node')
 
     # Initialize publisher for ur3/command with buffer size of 10
     pub_command = rospy.Publisher('ur3/command', command, queue_size=10)
 
-    # Initialize subscriber to ur3/position and callback fuction
+    # Initialize subscriber to ur3/position & ur3/gripper_input and callback fuction
     # each time data is published
     sub_position = rospy.Subscriber('ur3/position', position, position_callback)
-
-    ############## Your Code Start Here ##############
-    # TODO: define a ROS subscriber for ur3/gripper_input message and corresponding callback function
-    
-    sub_gripper = rospy.Subscriber('ur3/gripper_input', gripper_input, gripper_callback)
-
-
-    ############### Your Code End Here ###############
-
-
-    ############## Your Code Start Here ##############
-    # TODO: modify the code below so that program can get user input
-
-    input_done = 0
-    loop_count = 0
-    ABtower = 0
-    start_tower = 0
-    mid_tower = 0
-    end_tower = 0
-    # while(not input_done):
-    #     input_string = input("Enter number of loops <Either 1 2 3 or 0 to quit> ")
-    #     print("You entered " + input_string + "\n")
-
-
-    #     if(int(input_string) == 1):
-    #         input_done = 1
-    #         loop_count = 1
-    #     elif (int(input_string) == 2):
-    #         input_done = 1
-    #         loop_count = 2
-    #     elif (int(input_string) == 3):
-    #         input_done = 1
-    #         loop_count = 3
-    #     elif (int(input_string) == 0):
-    #         print("Quitting... ")
-    #         sys.exit()
-    #     else:
-    #         print("Please just enter the character 1 2 3 or 0 to quit \n\n")
-    while(not ABtower):
-        ABtower = input("Enter Starting and End Tower (1,2,3)")
-        ABtower = ABtower.strip()
-        start_tower = int(ABtower[0])-1
-        end_tower = int(ABtower[-1])-1
-        mid_tower = 3 -start_tower - end_tower
-
-
-
-
-    ############### Your Code End Here ###############
+    sub_input = rospy.Subscriber('ur3/gripper_input', gripper_input, input_callback)
 
     # Check if ROS is ready for operation
     while(rospy.is_shutdown()):
         print("ROS is shutdown!")
 
-    rospy.loginfo("Sending Goals ...")
-
+    # Initialize the rate to publish to ur3/command
     loop_rate = rospy.Rate(SPIN_RATE)
 
-    ############## Your Code Start Here ##############
-    # TODO: modify the code so that UR3 can move tower accordingly from user input
+    vel = 4.0
+    accel = 4.0
+    move_arm(pub_command, loop_rate, go_away, vel, accel)
 
-        
-    if (not move_block(pub_command, loop_rate, start_tower, 1, end_tower, 3)):
+    ic = ImageConverter(SPIN_RATE)
+    time.sleep(5)
+
+    # ========================= Student's code starts here =========================
+
+    """
+    Hints: use the found xw_yw_G, xw_yw_Y to move the blocks correspondingly. You will
+    need to call move_block(pub_command, loop_rate, start_xw_yw_zw, target_xw_yw_zw, vel, accel)
+    """
+    print(xw_yw_G)
+    i = 0
+    retval = 0
+    if( len(xw_yw_G) > 0):
+        for xy in xw_yw_G:
+            retval = move_block(pub_command,loop_rate,xy,(.24,.25+i),vel,accel)
+            
+            if(retval): 
+                print("ERROR: BLOCK NOT FOUND!")
+                return 0
+            i += .13
+    else:
         print("ERROR: BLOCK NOT FOUND!")
-        return 0
-    if (not move_block(pub_command, loop_rate, start_tower, 2, mid_tower, 3)):
-        print("ERROR: BLOCK NOT FOUND!")
-        return 0
-    if (not move_block(pub_command, loop_rate, end_tower, 3, mid_tower, 2)):
-        print("ERROR: BLOCK NOT FOUND!")
-        return 0
-    if (not move_block(pub_command, loop_rate, start_tower, 3, end_tower, 3)):
-        print("ERROR: BLOCK NOT FOUND!")
-        return 0
-    if (not move_block(pub_command, loop_rate, mid_tower, 2, start_tower, 3)):
-        print("ERROR: BLOCK NOT FOUND!")
-        return 0
-    if (not move_block(pub_command, loop_rate, mid_tower, 3, end_tower, 2)):
-        print("ERROR: BLOCK NOT FOUND!")
-        return 0
-    if (not move_block(pub_command, loop_rate, start_tower, 3, end_tower, 1)):
+        move_arm(pub_command, loop_rate, go_away, vel, accel)
+    i = 0 
+    if( len(xw_yw_Y) > 0):
+        for xy in xw_yw_Y:
+            retval = move_block(pub_command,loop_rate,xy,(.14,.25+i),vel,accel)
+            if(retval): 
+                print("ERROR: BLOCK NOT FOUND!")
+                move_arm(pub_command, loop_rate, go_away, vel, accel)
+                print("Use Ctrl+C to exit program")
+                rospy.spin()
+                return 0
+            i += .13
+    else:
         print("ERROR: BLOCK NOT FOUND!")
         return 0
 
     
-    # while(loop_count > 0):
+    # ========================= Student's code ends here ===========================
 
-    #     move_arm(pub_command, loop_rate, home, 4.0, 4.0)
-
-    #     rospy.loginfo("Sending goal 1 ...")
-    #     move_arm(pub_command, loop_rate, Q[0][0], 4.0, 4.0)
-
-    #     gripper(pub_command, loop_rate, suction_on)
-    #     # Delay to make sure suction cup has grasped the block
-    #     time.sleep(1.0)
-
-    #     rospy.loginfo("Sending goal 2 ...")
-    #     move_arm(pub_command, loop_rate, Q[1][1], 4.0, 4.0)
-
-    #     rospy.loginfo("Sending goal 3 ...")
-    #     move_arm(pub_command, loop_rate, Q[2][0], 4.0, 4.0)
-
-    #     loop_count = loop_count - 1
-
-    # gripper(pub_command, loop_rate, suction_off)
-
-
-
-    ############### Your Code End Here ###############
-
+    move_arm(pub_command, loop_rate, go_away, vel, accel)
+    rospy.loginfo("Task Completed!")
+    print("Use Ctrl+C to exit program")
+    rospy.spin()
 
 if __name__ == '__main__':
 
